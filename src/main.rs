d67f7cf54075ea435262ca1f645ad3010f8ac0bc
Julian Castano <julian@castano.app>
2024-01-02 15:43:04 -0500

    feat(core): Initial exploration of ratatui
    In this initial exploration we touch some of the ratatui components to
    render a basic pane with a custom title

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::stdout;
+
+use ratatui::backend::CrosstermBackend;
+use ratatui::layout::{Constraint, Direction, Layout};
+use ratatui::widgets::{Block, Borders};
+use ratatui::Terminal;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let backend = CrosstermBackend::new(stdout());
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(1)
+            .constraints([Constraint::Percentage(10), Constraint::Percentage(90)].as_ref())
+            .split(f.size());
+
+        let block = Block::default()
+            .title("DynamoDB Tables")
+            .borders(Borders::ALL);
+        f.render_widget(block, chunks[0]);
+    })?;
+
+    Ok(())
 }
