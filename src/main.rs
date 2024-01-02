1da6f6b1d049385a0b9fa09b1d9746f048de1b15
Julian Castano <julian@castano.app>
2024-01-02 16:22:11 -0500

    feat(core): Integrate aws dynamo db to list collections
    Add a basic function to load all the collections from aws and put them
    in a widget. AWS maximum size for the collections array is 100 so a way
    for it to filter and/or paginate results is required.

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,45 @@
 use std::io::stdout;
 
+use aws_config::meta::region::RegionProviderChain;
+use aws_config::BehaviorVersion;
+use aws_sdk_dynamodb::{Client, Error};
 use ratatui::backend::CrosstermBackend;
 use ratatui::layout::{Constraint, Direction, Layout};
-use ratatui::widgets::{Block, Borders};
+use ratatui::widgets::{Block, Borders, List, ListItem};
 use ratatui::Terminal;
+use tokio;
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    let config = aws_config::defaults(BehaviorVersion::latest())
+        .region(region_provider)
+        .load()
+        .await;
+    let client = Client::new(&config);
+
+    let table_names = list_tables(&client).await?;
+
     let backend = CrosstermBackend::new(stdout());
     let mut terminal = Terminal::new(backend)?;
 
     terminal.draw(|f| {
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints([Constraint::Percentage(10), Constraint::Percentage(90)].as_ref())
-            .split(f.size());
+        let items: Vec<ListItem> = table_names
+            .iter()
+            .map(|name| ListItem::new(name.clone()))
+            .collect();
 
-        let block = Block::default()
-            .title("DynamoDB Tables")
-            .borders(Borders::ALL);
-        f.render_widget(block, chunks[0]);
+        let list =
+            List::new(items).block(Block::default().borders(Borders::ALL).title("Collections"));
+
+        f.render_widget(list, f.size());
     })?;
 
     Ok(())
+}
+
+async fn list_tables(client: &Client) -> Result<Vec<String>, Error> {
+    let resp = client.list_tables().send().await?;
+    let table_names = resp.table_names().to_vec();
+    Ok(table_names)
 }
